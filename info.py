59acfb0f13f2fe9d1fe405a4807a0bfcd1d5ba7a
YTNOW <139692597+YTNOW@users.noreply.github.com>
2023-07-25 16:41:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://krishnanakshay652:krishnanakshay652@cluster0.tnh7usa.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://<username>:<password>@cluster0.bx08qe6.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "LazyDeveloper")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
