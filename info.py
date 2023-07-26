850efa921a9543053d49c236150c6c6d8080912c
YTNOW <139692597+YTNOW@users.noreply.github.com>
2023-07-26 17:59:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,8 +33,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Abhishek:Abhishek@cluster0.bang0b2.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Abhishek")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', '@ALAS_NOW')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
