0d0c0faf4e110fbab2fe74f7dffdf7e40b03e7b3
ALAS <139692597+YTNOW@users.noreply.github.com>
2023-07-27 16:39:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['23684657'])
+API_ID = int(environ['23684657']
 API_HASH = environ['471fbeeb71b5fbf9246fc7342bb6754a']
 BOT_TOKEN = environ['6445459577:AAEB5gI6STImQMr2B6ePWBaj5i68Q6DjLmA']
 
